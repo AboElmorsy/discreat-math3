3e2db4b376a1965d0ab4715763ecb83606271ac6
AboElmorsy <121674381+AboElmorsy@users.noreply.github.com>
2022-12-30 16:10:38 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace perfect_nombre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter first nombre");
+            int nom1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter seconde nombre");
+            int nom2 = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = nom1; i < nom2; i++)
+               
+            {
+                for (int x = 1; x <= i; x++)
+                {
+                    if (i % x == 0)
+                    {
+                        sum += x;
+                    }
+
+                    if (sum == i)
+                    {
+                        sum += i;
+
+                        Console.WriteLine(i);
+                    }
+                   
+                }
+                return ;
+            }
+            
+        }
+        }
+
+    }     
+        
+    
+
